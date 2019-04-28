16fd4f9cc77b5df635089384cff64071a8954dca
25279 <25279@LAPTOP-HO4KEV7S>
2019-04-28 19:39:33 +0800

    test

---
+++ src/hello/ExpressionTest.java
@@ -0,0 +1,19 @@
+package hello;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExpressionTest {
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+    void testSubtract() {
+        fail("Not yet implemented");
+    }
+
+}
